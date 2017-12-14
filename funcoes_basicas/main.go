7f7c4066381e138ad6bf108d148d2819cdfb604a
JordanaNog <jordana.eduzz@gmail.com>
2017-12-13 23:12:20 -0200

    secao 2 - aula 10

--- funcoes_basicas/main.go
+++ funcoes_basicas/main.go
@@ -10,12 +10,18 @@ func main(){
 	fmt.Printf("Resultado Multiplicação: %d\r\n", resultado)
 
 	resultado = matematica.Calculo(matematica.Soma, 2, 5)
-	fmt.Printf("Resultado Multiplicação: %d\r\n", resultado)
+	fmt.Printf("Resultado Soma: %d\r\n", resultado)
 
 	resultado = matematica.Calculo(matematica.Divisao, 2, 5)
-	fmt.Printf("Resultado Multiplicação: %d\r\n", resultado)
+	fmt.Printf("Resultado Divisao: %d\r\n", resultado)
 
 	resultado = matematica.Calculo(matematica.Subtracao, 2, 5)
-	fmt.Printf("Resultado Multiplicação: %d\r\n", resultado)
+	fmt.Printf("Resultado Subtracao: %d\r\n", resultado)
+
+	resultado = matematica.Somaaaa(8, 5)
+	fmt.Printf("Resultado da Somaaaa: %d\r\n", resultado)
+
+	resultado, resto := matematica.DivisaoComResto(9, 4)
+	fmt.Printf("Resultado da Divisão: %d\r\ne resultado com resto:  %d\r\n", resultado, resto)
 }
 
--- funcoes_basicas/matematica/operacoes.go
+++ funcoes_basicas/matematica/operacoes.go
@@ -13,10 +13,17 @@ func Soma(x int, y int) int {
 	return x + y
 }
 
-func Divisao(x int, y int) int {
-	return x % y
+func Divisao(x int, y int) (resultado int) {
+	resultado = x % y
+	return 
 }
 
 func Subtracao(x int, y int) int {
 	return x - y
+}
+
+func DivisaoComResto(x int, y int) (resultado int, resto int) {
+	resultado = x / y
+	resto = x % y
+	return 
 }
